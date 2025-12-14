ccf64bcf2d9b9936f0bc4a817ab6e5804d3d646c
Shubhamsoni024 <89872416+Shubhamsoni024@users.noreply.github.com>
2025-12-15 02:28:06 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,4 +2,9 @@
 set -e
 
 # Stop the running container (if any)
-echo "Hi"
+containerID=$("docker ps -a | awk -F " " '{print $1}' | grep -vi container")
+docker container stop -f ${containerID}
+docker conatiner rm -f ${containerID}
+
+# Removing the old images (if any)
+docker image prune -af
