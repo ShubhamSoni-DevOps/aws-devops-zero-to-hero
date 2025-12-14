613e26575319f367969545b100b615ba8e0ac5c5
Shubhamsoni024 <89872416+Shubhamsoni024@users.noreply.github.com>
2025-12-15 00:38:52 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull shubhamsonirg/simple-python-app
+docker pull shubhamsonirg/simple-python-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 shubhamsonirg/simple-python-app
+docker run -d -p 5000:5000 shubhamsonirg/simple-python-app:latest
