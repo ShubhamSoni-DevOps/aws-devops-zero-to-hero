d3366cf7ffd65b2709254d98e8483ab7f14ba8cb
Shubhamsoni024 <89872416+Shubhamsoni024@users.noreply.github.com>
2025-12-15 03:10:30 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "Stopping and removing all containers..."
-
 containers=$(docker ps -aq)
 
 if [ -n "$containers" ]; then
-  docker stop $containers
-  docker rm $containers
+  docker rm -f $containers
 else
-  echo "No containers found."
+  echo "No containers to remove."
 fi
 
-echo "Removing unused images..."
 docker image prune -af
-
-echo "Cleanup completed successfully."
